18c949c2c0222e42a885e0f07b917e5b238cc97a
yxdszlkc <yingzilkq@163.com>
2021-04-07 16:28:30 +0800

    添加图片验证码

--- blog/users/urls.py
+++ blog/users/urls.py
@@ -7,10 +7,13 @@
 
 # 进行users子应用的视图路由
 from django.urls import path
-from users.views import RegisterView
+from users.views import RegisterView, ImageCodeView
 
 urlpatterns = [
     # path的第一个参数，路由
     # PATH的第二个参数，视图函数名
     path('register/', RegisterView.as_view(), name='register'),
+
+    # 图片验证码的路由
+    path('imagecode/', ImageCodeView.as_view(), name='imagecode'),
 ]
