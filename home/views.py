e7b422eb8bd9128e16042d808dcd908152672dfa
影子 <3011456083@qq.com>
2021-09-29 22:41:33 +0800

    首页分类数据展示

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from home.models import ArticleCategory
+from home.models import ArticleCategory, Article
 from django.http.response import HttpResponseNotFound
 
 
@@ -12,7 +12,11 @@ class IndexView(View):
         1、获取所有分类信息
         2、接收用户点击的分类id
         3、根据分类id进行分类的查询
-        4、组织数据传递给模板
+        4.获取分页参数
+        5.根据分类信息查询文章数据
+        6.创建分页器
+        7.进行分页处理
+        8、组织数据传递给模板
         :param request:
         :return:
         """
@@ -25,9 +29,28 @@ class IndexView(View):
             category = ArticleCategory.objects.get(id=cat_id)
         except ArticleCategory.DoesNotExist:
             return HttpResponseNotFound('没有此分类')
-        # 4、组织数据传递给模板
+        # 4、获取分页参数
+        page_num = request.GET.get('page_num', 1)
+        page_size = request.GET.get('page_size', 10)
+        # 5.根据分类信息查询文章数据
+        articles = Article.objects.filter(category=category)
+        # 6.创建分页器
+        from django.core.paginator import Paginator, EmptyPage
+        paginator = Paginator(articles, per_page=page_size)
+        # 7.进行分页处理
+        try:
+            page_articles = paginator.page(page_num)
+        except EmptyPage:
+            return HttpResponseNotFound('empty page')
+        # 总页数
+        total_page = paginator.num_pages
+        # 8、组织数据传递给模板
         context = {
             'categories': categories,
-            'category': category
+            'category': category,
+            'articles': page_articles,
+            'page_size': page_size,
+            'total_page': total_page,
+            'page_num': page_num
         }
-        return render(request, 'index.html',context=context)
+        return render(request, 'index.html', context=context)
